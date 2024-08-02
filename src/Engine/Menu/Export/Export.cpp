00e47ba38b5e57588c8a766198bbdd126eb00ce3
SodiumNickel <139382010+SodiumNickel@users.noreply.github.com>
2024-08-02 11:15:40 -0400

    Quotes around filepath for exports

--- src/Engine/Menu/Export/Export.cpp
+++ src/Engine/Menu/Export/Export.cpp
@@ -22,7 +22,7 @@ void Menu::ExportPopup(){
 void Menu::ExportWindows(){
     // TODO: do the thing where we loop through (1), (2) until we find a valid path
     // Create directory for game inside of gameFilepath 
-    std::string dir = "mkdir " + engineData->gameFilepath + "\\\"" + engineData->gameName + "\"";
+    std::string dir = "mkdir \"" + engineData->gameFilepath + "\"\\\"" + engineData->gameName + "\"";
     int result = system(dir.c_str());
     if (result == 0) {
         std::cout << "Created dir successful!\n";
@@ -36,7 +36,7 @@ void Menu::ExportWindows(){
                       "Make/src/Game/Input/SaltyInput.cpp ";
     std::string inc = "-IMake/include/SDL2 -IMake/include/glm -IMake/include/nlohmann -IMake/src ";
     std::string lib = "-LMake/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image ";
-    std::string out = "-o " + engineData->gameFilepath + "/\"" + engineData->gameName + "\"/\"" + engineData->gameName + ".exe\"";
+    std::string out = "-o \"" + engineData->gameFilepath + "\"/\"" + engineData->gameName + "\"/\"" + engineData->gameName + ".exe\"";
 
     // TODO: do need a way to determine that user has proper packages installed (is it just mingw here?)
     std::string make = "g++ " + src + "-std=c++17 " + inc + lib + out;
@@ -51,7 +51,7 @@ void Menu::ExportWindows(){
 
     // Copy Unique folder
     // TODO: I would like to remove the printed console stuff
-    std::string copy = "xcopy /E /I /Y .\\Unique\\ " + engineData->gameFilepath + "\\\"" + engineData->gameName + "\"\\Unique";
+    std::string copy = "xcopy /E /I /Y .\\Unique\\ \"" + engineData->gameFilepath + "\"\\\"" + engineData->gameName + "\"\\Unique";
     result = system(copy.c_str());
     if (result == 0) {
         std::cout << "Copy unique successful!\n";
@@ -60,7 +60,7 @@ void Menu::ExportWindows(){
     }
 
     // Copy dll's
-    copy = "xcopy /I /Y .\\*.dll " + engineData->gameFilepath + "\\\"" + engineData->gameName + "\"";
+    copy = "xcopy /I /Y .\\*.dll \"" + engineData->gameFilepath + "\"\\\"" + engineData->gameName + "\"";
     result = system(copy.c_str());
     if (result == 0) {
         std::cout << "Copy dll's successful!\n";
@@ -72,7 +72,7 @@ void Menu::ExportWindows(){
 void Menu::ExportWeb(){
     // TODO: do the thing where we loop through (1), (2) until we find a valid path
     // Create directory for game inside of gameFilepath 
-    std::string dir = "mkdir " + engineData->gameFilepath + "\\\"" + engineData->gameName + "\"";
+    std::string dir = "mkdir \"" + engineData->gameFilepath + "\"\\\"" + engineData->gameName + "\"";
     int result = system(dir.c_str());
     if (result == 0) {
         std::cout << "Created dir successful!\n";
@@ -87,7 +87,7 @@ void Menu::ExportWeb(){
     std::string inc = "-IMake/include/SDL2 -IMake/include/glm -IMake/include/nlohmann -Iinclude/emscripten -IMake/src ";
     std::string wflags = "-s WASM=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s ALLOW_MEMORY_GROWTH=1 ";
     std::string preload = "--preload-file .\\Unique@Unique --use-preload-plugins ";
-    std::string out = "-o " + engineData->gameFilepath + "/\"" + engineData->gameName + "\"/index.html";
+    std::string out = "-o \"" + engineData->gameFilepath + "\"/\"" + engineData->gameName + "\"/index.html";
 
     // TODO: do need a way to determine that user has proper packages installed (is it just mingw here?)
     std::string make = "em++ " + src + "-std=c++17 " + inc + wflags + preload + out;
@@ -101,7 +101,7 @@ void Menu::ExportWeb(){
     }
 
     // Overwrite index.html - assuming we are building for itch.io so includes added window focus
-    std::string copy = "xcopy /I /Y .\\Make\\index.html " + engineData->gameFilepath + "\\\"" + engineData->gameName + "\"";
+    std::string copy = "xcopy /I /Y .\\Make\\index.html \"" + engineData->gameFilepath + "\"\\\"" + engineData->gameName + "\"";
     result = system(copy.c_str());
     if (result == 0) {
         std::cout << "Copy index successful!\n";
